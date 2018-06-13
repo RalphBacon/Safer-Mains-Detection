7bdc0e40f71995e1a12694d58e055e3c20b9a374
Ralph Bacon <arduino@jargongeneration.uk>
2018-06-13 19:23:19 +0100

    So simple - use with detector circuit in the video

---
+++ Mains_Detector.ino
@@ -0,0 +1,26 @@
+/*
+ * Simple mains detector
+ */
+
+#define pinVolts 2
+bool previousState = LOW;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinVolts, INPUT);
+  Serial.println("Setup Completed");
+}
+
+void loop() {
+  if (digitalRead(pinVolts) == HIGH && previousState == LOW) {
+    previousState = HIGH;
+    Serial.println("MAINS OFF");
+  }
+
+  if (digitalRead(pinVolts) == LOW && previousState == HIGH) {
+    previousState = LOW;
+    Serial.println("MAINS ON");
+  }
+
+  delay(250);
+}
